8630e53775c02717c6fb99fe1706213622fc5df2
Fifoblivion <fifoblivion9910@gmail.com>
2019-03-27 12:36:53 +0700

    fixing insertion sort

--- Source/insort.cpp
+++ Source/insort.cpp
@@ -1,49 +1,34 @@
-// Recursive C++ program for insertion sort
 #include <iostream>
 using namespace std;
 
-// Recursive function to sort an array using
-// insertion sort
-void insertionSortRecursive(int arr[], int n)
-{
-	// Base case
-	if (n <= 1)
-		return;
+void insertionSort(int x[], int n){
+  int j;
+  int insert;
 
-	// Sort first n-1 elements
-	insertionSortRecursive( arr, n-1 );
-
-	// Insert last element at its correct position
-	// in sorted array.
-	int last = arr[n-1];
-	int j = n-2;
-
-	/* Move elements of arr[0..i-1], that are
-	greater than key, to one position ahead
-	of their current position */
-	while (j >= 0 && arr[j] > last)
-	{
-		arr[j+1] = arr[j];
-		j--;
-	}
-	arr[j+1] = last;
+  for(int i=1; i < n; i++){
+    insert=x[i];
+    j=i-1;
+    while (j>=0 && x[j]>insert) {
+      x[j+1]= x[j];
+      j=j-1;
+    }
+    x[j+1]=insert;
+  }
+  cout<<"\nsorted output: ";
+  for (int i = 0; i < n; i++) {
+    cout<<x[i]<<" ";
+  }
+  cout<<endl;
 }
-
-// A utility function to print an array of size n
-void printArray(int arr[], int n)
-{
-	for (int i=0; i < n; i++)
-		cout << arr[i] <<" ";
-}
-
-/* Driver program to test insertion sort */
 int main()
 {
-	int arr[] = {12, 11, 13, 5, 6};
-	int n = sizeof(arr)/sizeof(arr[0]);
-
-	insertionSortRecursive(arr, n);
-	printArray(arr, n);
-
-	return 0;
+    int n; //banyak input
+    n=5;
+    int x[]={5,4,3,2,1};//array of input
+    cout<<"input: ";
+    for (int i = 0; i < n; i++) { //print array
+      cout<<x[i]<<" ";
+    }
+    insertionSort(x,n);
+    return 0;
 }
